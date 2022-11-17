507bb7e6d12423dcbd0b2c8331b14358b1f30fc5
Rakshay N <rakshay2108@gmail.com>
2022-11-17 17:35:05 -0500

    added arm class

---
+++ src/main/kotlin/frc/team449/robot2022/arm/Arm.kt
@@ -0,0 +1,48 @@
+package frc.team449.robot2022.arm
+
+import edu.wpi.first.math.controller.ProfiledPIDController
+import edu.wpi.first.math.controller.SimpleMotorFeedforward
+import edu.wpi.first.math.trajectory.TrapezoidProfile
+import edu.wpi.first.wpilibj.Timer
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.team449.system.motor.WrappedMotor
+
+
+class Arm(
+  private val armMotor: WrappedMotor
+  ): SubsystemBase() {
+
+  var lastSpeed: Double = 0.0
+  var lastTime: Double = Timer.getFPGATimestamp()
+
+  var controller: ProfiledPIDController = ProfiledPIDController(
+    ArmConstants.kP,
+    ArmConstants.kI,
+    ArmConstants.kD,
+    TrapezoidProfile.Constraints(ArmConstants.maxVel, ArmConstants.maxAccel)
+  )
+
+  var feedForward: SimpleMotorFeedforward = SimpleMotorFeedforward(ArmConstants.kS, ArmConstants.kV, ArmConstants.kA)
+
+  fun goToPos(desiredPos: Double) {
+    var goalState: TrapezoidProfile.State = TrapezoidProfile.State(desiredPos, 0.0)
+    controller.goal = goalState
+  }
+
+  fun togglePos() {
+    if (controller.goal == TrapezoidProfile.State(ArmConstants.hopperDesiredAngle, 0.0)) {
+      goToPos(ArmConstants.groundAngle)
+    }
+    else if (controller.goal == TrapezoidProfile.State(ArmConstants.groundAngle, 0.0)) {
+      goToPos(ArmConstants.hopperDesiredAngle)
+    }
+  }
+
+  override fun periodic() {
+    var accel: Double = (controller.setpoint.velocity - lastSpeed) / (Timer.getFPGATimestamp() - lastTime)
+    armMotor.setVoltage(
+      controller.calculate(armMotor.encoder.position) + feedForward.calculate(controller.setpoint.velocity, accel))
+    lastSpeed = controller.setpoint.velocity
+    lastTime = Timer.getFPGATimestamp()
+  }
+}
---
+++ src/main/kotlin/frc/team449/robot2022/arm/ArmConstants.kt
@@ -0,0 +1,25 @@
+package frc.team449.robot2022.arm
+
+object ArmConstants {
+
+
+  // PID controller values.
+  const val kP = 1.0
+  const val kI = 1.0
+  const val kD = 1.0
+
+
+  // Maximum velocity and acceleration.
+  const val maxVel = 1.0
+  const val maxAccel = 1.0
+
+
+  // Feed forward values.
+  const val kV = 1.0
+  const val kS = 1.0
+  const val kA = 1.0
+
+  const val hopperDesiredAngle = Math.PI / 2
+  const val groundAngle = 0.0
+
+}
