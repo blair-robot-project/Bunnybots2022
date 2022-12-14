e73d19aab9fef80447c43a092d692c4c05b042f0
Tewodros Mengistie <Tewodros.mengistie@gmail.com>
2022-12-13 22:00:49 -0500

    fix gyro

--- src/main/kotlin/frc/team449/system/AHRS.kt
+++ src/main/kotlin/frc/team449/system/AHRS.kt
@@ -18,7 +18,7 @@ class AHRS(private val navx: com.kauailabs.navx.frc.AHRS) : Gyro by navx, Loggab
       return -Rotation2d.fromDegrees(headingOffset + this.navx.fusedHeading)
     }
     set(newHeading) {
-      this.headingOffset = newHeading.degrees - this.navx.fusedHeading
+      this.headingOffset = -newHeading.degrees - this.navx.fusedHeading
     }
 
   constructor(port: SerialPort.Port = SerialPort.Port.kMXP) : this(com.kauailabs.navx.frc.AHRS(port))
